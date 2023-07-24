9aad55dfd935a17059db997196e1148758d5e40c
lukabakuradzee <130186938+lukabakuradzee@users.noreply.github.com>
2023-07-24 21:19:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,6 @@
 // });
 
 
-// submit buttonis refreshis gatishva
-
-// window.addEventListener('scroll', reveal);
-
-// function reveal()  {
-//   let reveals = document.querySelectorAll('.reveal')
-//   for (let i = 0; i < reveals.length; i++)  {
-//     let windowHeight = window.innerHeight;
-//     let revealTop = reveals[i].getBoundingClientRect().top;
-//     let revealPoint = 150;
-
-//     if(revealTop < windowHeight - revealPoint) {
-//       reveals[i].classList.add('active');
-//     }
-//     else {
-//       reveals[i].classList.remove('active');
-//     }
-//   }
-// }
-
 let mobileMenu = document.querySelector(".menu")
 let burgerMenu = document.querySelector(".burgermenu")
 let xmark = document.querySelector(".xmark")
