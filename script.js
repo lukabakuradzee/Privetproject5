5e45ebe54d39ee49d20f020aa38cb3e6af124af0
lukabakuradzee <130186938+lukabakuradzee@users.noreply.github.com>
2023-07-24 20:59:29 +0400

    page loader

--- script.js
+++ script.js
@@ -40,3 +40,36 @@ xmark.addEventListener("click", () => {
   xmark.style.display = "none";
 });
 
+
+
+
+// JavaScript code (file.js)
+
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll(".section1_slider, .section2_home, .home_articles, .section3_projects_complited, .section4_video_background, .article2, .article3, .section5, .section6_feedback, .section7_subscribe_newsletter, .footer1, .section1_about_page, .section2_about_page, .section3_about_page, .section4_about_page, .section6_video_about, .section7_our_values, .section8_company_porfessionals, .section9_customers_questions, .section1_contact_page, .section2_contact, .iframe1, .section4_project_talk");
+  
+  // Configuration for the Intersection Observer
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.3, // Adjust this value as needed
+  };
+  
+  const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show2");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+  
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+});
+
+
+
+
+
