28218ab240c5f54a5c59ffc59c5a8eba2a8e7a6d
lukabakuradzee <130186938+lukabakuradzee@users.noreply.github.com>
2023-07-25 19:37:59 +0400

    updated 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-
+function setupMobileMenu () {
 let mobileMenu = document.querySelector(".menu")
 let burgerMenu = document.querySelector(".burgermenu")
 let xmark = document.querySelector(".xmark")
@@ -9,6 +9,7 @@ burgerMenu.addEventListener("click", () => {
   mobileMenu.classList.add("show");
   burgerMenu.style.display = "none";
   xmark.style.display = "block";
+
 });
 
 
@@ -20,44 +21,57 @@ xmark.addEventListener("click", () => {
 
 
 
+}
+
+
+
+
+
 
 // JavaScript code (file.js)
-
-document.addEventListener("DOMContentLoaded", function () {
+function setupIntersectionObserver () {
   const sections = document.querySelectorAll(".section1_slider, .section2_home, .home_articles, .section3_projects_complited, .section4_video_background, .article2, .article3, .section5, .section6_feedback, .section7_subscribe_newsletter, .footer1, .section1_about_page, .section2_about_page, .section3_about_page, .section4_about_page, .section6_video_about, .section7_our_values, .section8_company_porfessionals, .section9_customers_questions, .section1_contact_page, .section2_contact, .iframe1, .section4_project_talk");
   
   // Configuration for the Intersection Observer
   const options = {
     root: null,
     rootMargin: "0px",
-    threshold: 0.3, // Adjust this value as needed
+    threshold: 0.10, // Adjust this value as needed
   };
-  
+
   const observer = new IntersectionObserver(function (entries, observer) {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("show2");
+        entry.target.classList.add("load");
         observer.unobserve(entry.target);
       }
     });
   }, options);
-  
+
   sections.forEach((section) => {
     observer.observe(section);
   });
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  setupMobileMenu();
+  setupIntersectionObserver();
 });
 
 
 
-// JavaScript code
-const submitBTN = document.getElementById('send_message_button');
 
-submitBTN.addEventListener('click', event => {
-  event.preventDefault();
-  event.stopImmediatePropagation();
+  // JavaScript code
+  // const submitBTN = document.getElementById('send_message_button');
 
-  // Your specific actions related to preventing the default action, if needed
-  // If not, you can omit this part.
-});
+  // submitBTN.addEventListener('click', event => {
+  //   event.preventDefault();
+  //   event.stopImmediatePropagation();
+
+  //   // Your specific actions related to preventing the default action, if needed
+  //   // If not, you can omit this part.
+  // });
 
 
